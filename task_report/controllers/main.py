d3ed7a60f1ec93445916a1c779a27ecb602df8c6
mahmoud-youssef <mahmoudyelbadry@gmail.com>
2023-01-25 13:59:19 +0300

    Simplify The Json

--- task_report/controllers/main.py
+++ task_report/controllers/main.py
@@ -1,7 +1,10 @@
 import logging
 import json
-from odoo import http
-from odoo.http import request, Response
+
+from openid import oidutil
+
+import openerp.http as http
+from openerp.http import request, Response
 
 _logger = logging.getLogger(__name__)
 oidutil.log = _logger.debug
@@ -10,9 +13,20 @@ class OpenIDController(http.Controller):
 
     @http.route('/report-task', auth='none', type='http', method=['GET'], cors='*')
     def process(self, **kw):
-        partners = request['res.partners'].sudo().search_read([])
+        data = []
+        # WE Can make an auth here or pagination
+        tasks = request.env['project.task'].sudo().search([])
+        for task in tasks:
+            subData = {
+                'user_id': task.user_id.id,
+                'project_id': task.project_id.id,
+                'name': task.user_id.name,
+                'delay_tasks': task.delay_tasks
+            }
+            if subData not in data:
+                data.append(subData)
 
         headers = {'Content-Type': 'application/json'}
-        body = {'results': {'code': 200, 'message': partners}}
+        body = {'results': {'code': 200, 'message': data}}
 
         return Response(json.dumps(body), headers=headers)
