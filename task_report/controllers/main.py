972601e05f63b825120c6631c474ac1c8a312be3
mahmoud-youssef <mahmoudyelbadry@gmail.com>
2023-01-25 22:08:38 +0300

    UI Implementation and new post man collection

--- task_report/controllers/main.py
+++ task_report/controllers/main.py
@@ -12,7 +12,7 @@ oidutil.log = _logger.debug
 class OpenIDController(http.Controller):
 
     @http.route('/report-task', auth='none', type='http', method=['GET'], cors='*')
-    def process(self, **kw):
+    def ReportTasks(self, **kw):
         data = []
         users_ids = []
         # WE Can make an auth here or pagination
@@ -33,12 +33,41 @@ class OpenIDController(http.Controller):
                         projects_ids.append(task_per_user.project_id.id)
                         subDataPro = {
                                 'id': task_per_user.project_id.id,
+                                'name': task_per_user.project_id.name,
+                                'open_tasks': task_per_user.open_tasks,
                                 'delay_tasks': task_per_user.delay_tasks,
                                 'past_week_finished_tasks': task_per_user.past_week_finished_tasks,
                                 'current_month_finished_tasks': task_per_user.current_month_finished_tasks,
                         }
                         subData['projects'].append(subDataPro)
                 data.append(subData)
+
+        headers = {'Content-Type': 'application/json'}
+        body = {'results': {'code': 200, 'message': data}}
+
+        return Response(json.dumps(body), headers=headers)
+
+    @http.route('/user-tasks/<int:rec_id>', auth='none', type='http', method=['GET'], cors='*')
+    def UserTasks(self, rec_id ,**kw):
+        data = []
+        users_ids = []
+        # WE Can make an auth here or pagination
+        tasks = request.env['project.task'].sudo().search([('user_id','=',rec_id)])
+        data = {
+            'user_id': tasks[0].user_id.id,
+            'projects': [],
+            'name': tasks[0].user_id.name,
+            'image': tasks[0].user_id.image
+        }
+        for task in tasks:
+            subData = {
+                    'name': task.project_id.name,
+                    'stage_id': task.stage_id.name,
+                    'progress': task.progress,
+                    'task_name': task.name,
+                    'deadline': task.name,
+                }
+            data['projects'].append(subData)
 
         headers = {'Content-Type': 'application/json'}
         body = {'results': {'code': 200, 'message': data}}
